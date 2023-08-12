c1fccbe20f17a56ce52396f83bbaf34690399b85
Natanael Girón <70552363+Nate20029@users.noreply.github.com>
2023-08-11 20:36:53 -0600

    ruido hamming

--- Lab2.2 Hamming/emisor2.py
+++ Lab2.2 Hamming/emisor2.py
@@ -1,7 +1,12 @@
 import numpy as np
 import socket
 
-def emisor_bits(data_bits):
+def apply_noise(bit, error_prob):
+    if np.random.rand() < error_prob:
+        return 1 - bit
+    return bit
+
+def emisor_bits_with_noise(data_bits, error_prob):
     n = len(data_bits)
     m = 1
     while 2 ** m < n + m + 1:
@@ -22,10 +27,19 @@ def emisor_bits(data_bits):
         xor_val = np.bitwise_xor.reduce(hamming_code[np.arange(1, n + m + 1) & (2 ** i) == 2 ** i])
         hamming_code[2 ** i - 1] = xor_val
 
-    return hamming_code.tolist()
+    # Decide un índice aleatorio para aplicar el ruido
+    noise_index = np.random.randint(0, len(hamming_code))
+    hamming_code_with_noise = np.array([apply_noise(bit, error_prob) if idx == noise_index else bit for idx, bit in enumerate(hamming_code)])
+
+    return hamming_code_with_noise.tolist()
 
 try:
     input_word = input("Ingrese una palabra o letra: ")
+    error_prob = float(input("Ingrese la probabilidad de error (entre 0 y 1): "))
+
+    if error_prob < 0 or error_prob > 1:
+        raise ValueError("La probabilidad de error debe estar entre 0 y 1")
+
     ascii_data = [ord(char) for char in input_word]
     binary_data = [format(code, '08b') for code in ascii_data]
     data_bits = np.array([int(bit) for code in binary_data for bit in code])
@@ -36,17 +50,21 @@ try:
     print("\nASCII convertido a bits:")
     print(binary_data)
 
-    hamming_code = emisor_bits(data_bits)
-    hamming_code_str = ''.join(map(str, hamming_code))
+    hamming_code_str = emisor_bits_with_noise(data_bits, error_prob)
 
-    print("\nSecuencia de código de Hamming a enviar:")
-    print(hamming_code_str)
+    print("\nSecuencia de código de Hamming original:")
+    print(''.join(map(str, hamming_code_str)))
+
+    hamming_code_with_noise = [apply_noise(int(bit), error_prob) for bit in hamming_code_str]
+
+    print("\nSecuencia de código de Hamming a enviar con ruido:")
+    print(''.join(map(str, hamming_code_with_noise)))
 
     host = '127.0.0.1'
     port = 12345
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        s.sendall(hamming_code_str.encode())
+        s.sendall(''.join(map(str, hamming_code_with_noise)).encode())
 
 except ValueError as e:
     print("\nError:", str(e))
--- Lab2.2 Hamming/index.js
+++ Lab2.2 Hamming/index.js
@@ -1,75 +1,112 @@
 const net = require('net');
 
-function decodeHamming(encodedBits) {
-    const n = encodedBits.length;
-    let m = 1;
-    while (Math.pow(2, m) < n + m + 1) {
-        m++;
-    }
-
-    const decodedBits = new Array(n - m);
-
-    let j = 0;
-    for (let i = 0; i < n; i++) {
-        if ((i & (i + 1)) !== 0) {
-            decodedBits[j] = encodedBits[i];
-            j++;
-        }
-    }
+// Función para detectar y corregir errores en el código de Hamming
+function detectAndCorrectErrors(hammingCode) {
+    const m = Math.floor(Math.log2(hammingCode.length + 1));
+    const errorBits = [];
+    let parityBitSum = 0;
 
     for (let i = 0; i < m; i++) {
-        let xorVal = 0;
-        for (let k = 0; k < n; k++) {
-            if (((k + 1) & (1 << i)) === (1 << i)) {
-                xorVal ^= encodedBits[k];
+        const parityIndex = Math.pow(2, i) - 1;
+
+        for (let j = parityIndex; j < hammingCode.length; j = j + (2 * parityIndex) + 2) {
+            for (let k = 0; k <= parityIndex && j + k < hammingCode.length; k++) {
+                parityBitSum ^= hammingCode[j + k];
             }
         }
-        if (xorVal !== 0) {
-            console.log(`Error en el bit de paridad ${i + 1}`);
+
+        if (parityBitSum !== 0) {
+            errorBits.push(parityIndex);
+        }
+        
+        parityBitSum = 0;
+    }
+
+    if (errorBits.length > 0) {
+        console.log(`Se encontraron errores en los bits: ${errorBits.join(', ')}`);
+        for (const errorBit of errorBits) {
+            hammingCode[errorBit] ^= 1;
+        }
+    } else {
+        console.log('No se encontraron errores en el código de Hamming.');
+    }
+
+    return hammingCode;
+}
+
+// Función para decodificar el código de Hamming a binario
+function decodeHammingToBinary(hammingCode) {
+    const m = Math.floor(Math.log2(hammingCode.length + 1));
+    const dataBits = [];
+
+    for (let i = 0; i < hammingCode.length; i++) {
+        if ((i & (i + 1)) !== 0) {
+            dataBits.push(hammingCode[i]);
         }
     }
 
-    return decodedBits;
+    return dataBits;
 }
 
-const server = net.createServer(socket => {
-    let receivedData = '';
+// Función para convertir binario a valores ASCII
+function binaryToASCII(binaryArray) {
+    const asciiValues = [];
 
-    socket.on('data', data => {
-        receivedData += data.toString();
+    for (let i = 0; i < binaryArray.length; i += 8) {
+        const byte = binaryArray.slice(i, i + 8).join('');
+        const decimalValue = parseInt(byte, 2);
+        asciiValues.push(decimalValue);
+    }
+
+    return asciiValues;
+}
+
+// Función para convertir valores ASCII a texto
+function asciiToText(asciiValues) {
+    let text = '';
+
+    for (const asciiValue of asciiValues) {
+        const char = String.fromCharCode(asciiValue);
+        text += char;
+    }
+
+    return text;
+}
+
+// Crear un servidor TCP para recibir el código de Hamming con ruido
+const server = net.createServer((socket) => {
+    console.log('Cliente conectado');
+    let hammingCodeWithNoise = '';
+
+    socket.on('data', (data) => {
+        hammingCodeWithNoise += data.toString();
     });
 
     socket.on('end', () => {
-        const receivedBits = receivedData.split('').map(bit => parseInt(bit, 10));
-        const decodedBits = decodeHamming(receivedBits);
-        
-        const asciiCodes = [];
-        for (let i = 0; i < decodedBits.length; i += 8) {
-            const byte = decodedBits.slice(i, i + 8);
-            const asciiCode = parseInt(byte.join(''), 2);
-            asciiCodes.push(asciiCode);
-        }
-
-        console.log('\nCodigo de Hamming Recibido:');
-        console.log(receivedBits.join(''));
-
-        console.log('\nCodigo de Hamming decodificado (ASCII a binario):');
-        console.log(decodedBits.join(''));
-
-        console.log('\nValor binario a decimal:');
-        for (let i = 0; i < decodedBits.length; i += 8) {
-            const byte = decodedBits.slice(i, i + 8);
-            console.log(byte.join('') + ' -> ' + parseInt(byte.join(''), 2));
-        }
-
-        console.log('\nValor decimal a letra o palabra:');
-        for (const asciiCode of asciiCodes) {
-            console.log(asciiCode + ' -> ' + String.fromCharCode(asciiCode));
-        }
-
-        const receivedText = String.fromCharCode(...asciiCodes);
-        console.log('\nTexto recibido:');
-        console.log(receivedText);
+        console.log('\nSecuencia de código de Hamming con ruido recibida:');
+        console.log(hammingCodeWithNoise);
+    
+        const receivedCode = hammingCodeWithNoise.split('').map(Number);
+    
+        // Detectar y corregir errores en el código de Hamming
+        const correctedCode = detectAndCorrectErrors(receivedCode);
+        console.log('\nSecuencia de código de Hamming corregida:');
+        console.log((correctedCode.join('')));
+    
+        // Decodificar el código de Hamming a binario
+        const decodedBinary = decodeHammingToBinary(correctedCode);
+        console.log('\nCódigo de Hamming decodificado a binario:');
+        console.log(decodedBinary.join(''));
+    
+        // Convertir binario a valores ASCII
+        const asciiValues = binaryToASCII(decodedBinary);
+        console.log('\nValores ASCII:');
+        console.log(asciiValues.join(', '));
+    
+        // Convertir valores ASCII a texto
+        const decodedText = asciiToText(asciiValues);
+        console.log('\nTexto decodificado:');
+        console.log(decodedText);
     });
 });
 
